c437202058082b976150acf64f51985aee549002
Moe95-bit <sheikha27@gmail.com>
2022-01-14 15:41:31 +0100

    Add project files.

---
+++ CV_Webbutveckling/Controllers/CvController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CV_Webbutveckling.Models;
+
+namespace CV_Webbutveckling.Controllers
+{
+    public class CvController : Controller
+    {
+        public IActionResult OmMig()
+        {
+            return View();
+        }
+
+        public IActionResult Erfarenhet()
+        {
+            return View();
+        }
+
+        public IActionResult Utbildning()
+        {
+            return View();
+        }
+
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        public IActionResult personligtBrev()
+        {
+            return View();
+        }
+    }
+}
---
+++ CV_Webbutveckling/Models/MittCV.cs
@@ -0,0 +1,60 @@
+﻿namespace CV_Webbutveckling.Models
+{
+    public class MittCV
+    {
+        public static string[] OmMig { get; } = new string[] {"Jag är en nuvarande student på Gävle " +
+            "Högskolan där jag studerar till att bli en dataingenjör. " +
+            "Jag har alltid varit intresserad av teknik, både den fysiska hårdvaran och mjukvaran. " +
+            "Jag letar just nu efter en anställning som kan hjälpa mig utvecklas vidare " +
+            "samtidigt som tillåter mig balansera mina studier."};
+        public static string[] Erfarenhet { get; } = new string[] {"Hade i uppgift att se till att resenärer på Arlanda " +
+            "flygplats följde de lagar och regler som fanns satta av Länsstyrelsen, Transportstyrelsen och polisen. " +
+            "Hade även lite mindre uppgifter på Bromma flygplats Fick lära mig mycket om konflikthantering, " +
+            "samt att behålla det lugnt när det var mycket stress.", 
+            
+            "Nokas tog över säkerheten på Arlanda flygplats från Securitas och blev anställd under en ny arbetsgivare. " +
+            "Hade i uppgift att se till att resenärer på Arlanda flygplats följde de lagar och regler som fanns " +
+            "satta av Länsstyrelsen, Transportstyrelsen och polisen. " +
+            "Fick lära mig mycket om konflikthantering, samt att " +
+            "behålla ett lugn när det var mycket stress.", 
+            
+            "Jobbade som vaktmästare parallellt " +
+            "med plugget under min tid på högstadiet och gymnasiet. " +
+            "Arbetade på flera olika ställen, där jag " +
+            "fick bl.a. lära migen massa om kundbemötande och kundkontakt. " +
+            "Jobbade för det mesta självständigt, vilket ledde till " +
+            "en utvecklad disciplinsförmåga då jag fick ta an mig mycket ansvar.", 
+
+            "Under sommarlovet från plugget arbeta jag som kundtjänst På open infra AB. " +
+            "Där fick jag besvara mejl och ta emot samtal från kunder som hade " +
+            "frågor angående sina avtal eller angående deras " +
+            "fiberuppkoppling. Fick även hantera fakturor och uppdatera " +
+            "uppgifter om nya och gamla kunder samt att försöka sälja " +
+            "fiber till kunder som visa intresse och hantera reklamationer."};
+        public static string[] Utbildning { get; } = new string[] {"Nu går jag Dataingenjörsprogrammet, " +
+            "påbörjade studierna 2020 och beräknas vara färdig 2023. " +
+            "Fokus på C# med .NET som hittills gått väldigt bra och har lärt mig " +
+            "en massa nytt. Vi har följande kurser i utbildningen, Programmeringsteknik " +
+            "C#, Grunder i JavaScript, Webbutveckling backend" +
+            "Webbutveckling frontend, Utveckling med ramverket .Net, Dataåtkomster i " +
+            ".Net, Projekt, organisation och äffärsmannaskap, Utveckling i Windows molntjänster och" +
+            " Databaser med SQL samt två tillfällen där vi har LIA (Lärande i arbete), " +
+            "följande kurser är genomförda SQL, " +
+            "JavaScript, Dataåtkomster i .Net, C#, Webbutveckling backend och frontend."};
+
+        public static string[] Kontakt { get; } = new string[] {"076-298 87 50 ", "mohammad.sheikha@outlook.com ", "Krusenberg 20, 755 98 Uppsala, Sverige"};
+
+        public static string[] personligtBrev { get; } = new string[] {"Omtänksam, trevlig, glad och " +
+            "väldigt noggrann, det är tre ord som beskriver mig ganska bra skulle jag säga. " +
+            "Jag pluggar till att bli programmerare på Gävle högskola där jag läser DOT-NET utvecklare " +
+            "programmet. Vi har en praktisk kurs på skolan, " +
+            "LIA (Lärande I Arbete) där studenterna ansöker till praktik på företag för att få mer " +
+            "arbetslivserfarenhet när man väl ska börja jobba som " +
+            "utvecklare efter studierna. Jag har läst om företaget och det lät som ett väldigt " +
+            "intressant och ett trevligt arbetsplats som skulle passa mig och där " +
+            "jag kan vidareutvecklas som en framtida utvecklare. Återkom gärna om ni har möjlighet " +
+            "att erbjuda mig en praktikplats hos er, ser fram emot eran återkoppling. " +
+            "Fritiden jag har spenderar jag mest på att plugga, träna, spendera tid med " +
+            "familjen och ägna tid till mina hobby programmering,data bygge och det mesta inom IT och datorer."};
+    }
+}
